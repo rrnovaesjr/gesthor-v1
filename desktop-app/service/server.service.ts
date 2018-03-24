6ec73004780feae075f6875f4cff51389b359308
rodrigo <rodrigo.novaes.jr@gmail.com>
2018-03-24 11:48:34 -0300

    Creation of constant utilities and Multi-server service

--- desktop-app/service/server.service.ts
+++ desktop-app/service/server.service.ts
@@ -7,26 +7,44 @@ import { Server } from 'http';
 export namespace ServerService {
 
     /**
-     * Global reference to the server application.
+     * Global reference to the application's servers.
      */
-    export var serverApplication: express.Express = express();
+    var serverApplication: Map<number, express.Express> = new Map();
 
     /**
      * Receives a handler or an array of handlers to be used as callbacks for application's servers.
      * 
+     * The correspondant server is identified by the port its listening.
+     * 
+     * @param port A port number.
      * @param handler Defines a request handler to be used on callbacks.
      */
-    export function use(handler: express.RequestHandler | express.RequestHandler[]): express.Express {
-        return serverApplication.use(handler);
+    export function use(port: number, handler: express.RequestHandler | express.RequestHandler[]): express.Express {
+        if(!serverApplication.get(port)) {
+            return null;
+        }
+        return serverApplication.get(port).use(handler);
     }
 
     /**
-     * Sets the server application to listen on a specific port.
+     * Sets a server application to listen on a specific port.
+     * 
+     * @param port A port number.
+     * @param hostname A string to the hostname.
+     * @param callback A function to be executed as callback.
+     */
+    export function listen(port: number, hostname?: string, callback?: Function): Server {
+        serverApplication.set(port, express());
+        return serverApplication.get(port).listen(port, hostname, callback);
+    }
+
+    /**
+     * Returns an express object from a given port number.
      * 
      * @param port A port number.
      */
-    export function listen(port: number, hostname?: string, callback?: Function): Server {
-        return serverApplication.listen(port, hostname, callback);
+    export function getExpressByPort(port: number): express.Express {
+        return serverApplication.get(port);
     }
 
 }
---
+++ desktop-app/service/util/constants.ts
@@ -0,0 +1,29 @@
+
+
+/**
+ * A global reference to the application's constants.
+ * 
+ * @author rodrigo-novaes
+ */
+export namespace Constants {
+
+    /**
+     * The default API's port.
+     */
+    export const DEFAULT_API_PORT: number = 8080;
+
+    /**
+     * The default Angular app's port.
+     */
+    export const DEFAULT_APP_PORT: number = 9000;
+
+    /**
+     * The object that defines the development environment.
+     */
+    export const ENVIRONMENT_DEV: string = 'dev';
+
+    /**
+     * The object that defines the production environment.
+     */
+    export const ENVIRONMENT_PROD: string = 'prod';
+}
