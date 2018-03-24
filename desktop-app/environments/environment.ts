e4f4ed074d93bc24c20a951070888b7acf04427e
rodrigo <rodrigo.novaes.jr@gmail.com>
2018-03-24 14:25:41 -0300

    Environment's last configuration

--- desktop-app/environments/environment.ts
+++ desktop-app/environments/environment.ts
@@ -4,7 +4,29 @@ export const environment = {
       host: 'localhost',
       user: 'root',
       database: 'gesthor_dev',
-      password: '1234'
+      password: '123456'
+    },
+    auth: {
+      secret: {
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: "https://rrnovaesjr.auth0.com/.well-known/jwks.json"
+      },
+      audience: 'http://localhost:8080',
+      issuer: 'https://rrnovaesjr.auth0.com/',
+      algorithms: ['RS256']
+    },
+    serverConfig: {
+      bodyParser: {
+        json: {
+          limit: '50mb'
+        },
+        urlencoded: {
+          extended: true
+        },
+        static: 'public'
+      }
     }
   };
   
