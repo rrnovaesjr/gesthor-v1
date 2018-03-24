2b433817a07ff965c38ddeffc2dbbdcd4e589414
rodrigo <rodrigo.novaes.jr@gmail.com>
2018-03-24 15:17:12 -0300

    Corrections on interfaces

--- desktop-app/repository/mysql-repository.interface.ts
+++ desktop-app/repository/mysql-repository.interface.ts
@@ -1,3 +1,4 @@
+import { QueryError, RowDataPacket } from 'mysql2';
 
 /**
  * A default interface for a repository object.
@@ -6,7 +7,7 @@
  * 
  * @author rodrigo-novaes
  */
-export interface MySQLRepository<E, PK> {
+export interface MySQLRepository<E, _E extends RowDataPacket, PK> {
 
     /**
      * Creates an entity.
@@ -14,8 +15,9 @@ export interface MySQLRepository<E, PK> {
      * Can throw exceptions based on multiple identifiers definitions.
      * 
      * @param entity Entity to be persisted.
+     * @param callback A function to be executed when create is done.
      */
-    create(entity: E): E;
+    create(entity: E, callback: (err: QueryError, result: _E | _E[]) => any): void;
 
     /**
      * Updates an entity.
@@ -23,30 +25,42 @@ export interface MySQLRepository<E, PK> {
      * Can throw exceptions based on null identifiers.
      * 
      * @param entities Entity to be updated.
+     * @param callback A function to be executed when the update is completed.
      */
-    update(entity: E): E;
+    update(entity: E, callback: (err: QueryError, result: _E | _E[]) => any): void;
 
     /**
      * Reads an entity based on its primary key.
      * 
      * @param id A unique identifier.
+     * @param callback A function to be executed when the search is completed.
      */
-    findOne(id: PK): E;
+    findOne(id: PK, callback: (err: QueryError, result: _E | _E[]) => any): void;
 
     /**
      * Find all objects from this entity.
      * 
      * It's possible to insert query params to filter results as needed.
      * 
+     * @param callback A function to be executed when the query is completed.
      * @param params Optional query params.
      */
-    findAll(params?: any[]): E[];
+    findAll(callback: (err: QueryError, result: _E | _E[]) => any, params?: any[]): void;
+
+    /**
+     * Deletes an entity based on its primary key.
+     * 
+     * @param id A unique identifier.
+     * @param callback A function to be executed when the deletion is completed.
+     */
+    delete(id: PK, callback: (err: QueryError, result: _E | _E[]) => Query): void;
 
     /**
      * Finds objects based on optional search parameters.
      * 
+     * @param callback A function to be executed when the search is completed.
      * @param params Optional query params.
      * @param searchOptions Optional search parameters.
      */
-    search(params?: any[], searchOptions?: any[]): E[];
+    search(callback: (err: QueryError, result: _E | _E[]) => any, params?: any[], searchOptions?: any[]): void;
 }
--- desktop-app/service/rest-service.interface.ts
+++ desktop-app/service/rest-service.interface.ts
@@ -1,12 +1,30 @@
+import { Request, Response } from 'express';
 
+/**
+ * A default interface for a REST API service.
+ * 
+ * @author rodrigo-novaes
+ */
 export interface RestAPIService {
 
+    /**
+     * A set of pairs (`url`, `callback`) that maps a HTTP POST request urls to a specific URL.
+     */
     post?: Function[];
 
+    /**
+     * A set of pairs (`url`, `callback`) that maps a HTTP PUT request urls to a specific URL.
+     */
     put?: Function[];
+    
+    /**
+     * A set of pairs (`url`, `callback`) that maps a HTTP GET request urls to a specific URL.
+     */
+    get?: { url: string, callback: (req: Request, res: Response) => void}[];
 
-    get?: { url: string, callback: (req, res, next) => void}[];
-
+    /**
+     * A set of pairs (`url`, `callback`) that maps a HTTP DELETE request urls to a specific URL.
+     */
     delete?: Function[];
 
 }
