35b4254ca40c982d1f0f23772c0d60d257f40c47
rodrigo <rodrigo.novaes.jr@gmail.com>
2018-03-28 08:22:19 -0300

    Stable version of member's module

--- src/app/member/member.module.ts
+++ src/app/member/member.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ClickOutsideModule } from 'ng-click-outside';
 import { MemberComponent } from './member.component';
 import { SharedModule } from '../shared';
 
 @NgModule({
   imports: [
+    ClickOutsideModule,
     SharedModule
   ],
   declarations: [
