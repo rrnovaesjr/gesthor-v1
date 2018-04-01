87afd16ba5d202569e7e6af86b36be3fcb6e906f
rodrigo <rodrigo.novaes.jr@gmail.com>
2018-04-01 16:18:21 -0300

    Progress Advisor Module
    A global progress bar.

--- src/app/progress-advisor/progress-advisor.module.ts
+++ src/app/progress-advisor/progress-advisor.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared';
 import { ProgressAdvisorComponent } from './progress-advisor.component';
+import { ProgressAdvisorService } from './progress-advisor.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule
   ],
-  declarations: [ProgressAdvisorComponent]
+  declarations: [
+    ProgressAdvisorComponent
+  ],
+  exports: [
+    ProgressAdvisorComponent
+  ],
+  providers: [
+    ProgressAdvisorService
+  ]
 })
 export class ProgressAdvisorModule { }
