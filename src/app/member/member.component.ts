58feffbda163401c28a37da28a075019a16c5545
rodrigo <rodrigo.novaes.jr@gmail.com>
2018-04-08 11:43:43 -0300

    New vision of member's component

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -12,66 +12,8 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-member',
-  template: `
-  <div (clickOutside)="onClickOutside($event)">
-    <button mat-icon-button (click)="toggle()"><mat-icon>account_circle</mat-icon></button>
-    <br>
-    <div class="login-card-container">
-      <mat-card class="login-card" *ngIf="showCard" [@enterCard]>
-        <div *ngIf="!authService.isAuthenticated">
-          <mat-card-header>
-            <img mat-card-avatar src="assets/img/no-user.png" />
-            <mat-card-title>{{ 'member.bemVindo' | translate }}</mat-card-title>
-          </mat-card-header>
-          <br>
-          <mat-card-content class="login-content">
-            <p>{{ 'member.loginHeader' | translate }}</p>
-            <p>{{ 'member.loginBody' | translate }}</p>
-          </mat-card-content>
-          <mat-card-actions>
-            <button mat-button (click)="authService.login()">{{ 'global.login' | translate }}</button>
-          </mat-card-actions>
-        </div>
-        <div *ngIf="authService.isAuthenticated">
-          <mat-card-header>
-            <img mat-card-avatar [src]="user?.picture" />
-            <mat-card-title>{{user?.name}}</mat-card-title>
-            <mat-card-subtitle><small>{{user?.email}}</small></mat-card-subtitle>
-          </mat-card-header>
-          <br>
-          <mat-card-content>
-
-          </mat-card-content>
-          <mat-card-actions>
-            <button mat-button (click)="authService.logout()">{{ 'global.logout' | translate }}</button>
-          </mat-card-actions>
-        </div>
-      </mat-card>
-    </div>
-  </div>
-  `,
-  styles: [
-    `:host p {
-      white-space: pre-line;
-      text-align: justify;
-    }`,
-    `.login-card-container {
-      position: relative;
-      right: 200px;
-    }`, 
-    `.login-card {
-      position: fixed;
-      z-index: 2;     
-      width: 200px;
-      line-break: normal;
-    }`,
-    `.login-card img {
-      height: 50px;
-      width: 50px;
-      padding: 0;
-      margin: 0;
-    }`
-  ],
+  templateUrl: './member.component.html',
+  styleUrls: ['./member.component.css'],
   animations: [
     trigger('enterCard', [
       transition('void => *', [
@@ -103,8 +45,7 @@ export class MemberComponent implements OnInit {
    */
   constructor(public authService: AuthService, private translateService: TranslateService) {
     this.authService.userProfile$.subscribe((res: Auth0UserProfile) => {
-      this.user = res;
-      
+      this.user = res;      
     });
   }
 
