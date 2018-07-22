b00b67cf1dc3b9a983ccfffa32a5dd5dc2192e72
Rodrigo Novaes <rodrigo.novaes.jr@gmail.com>
2018-07-21 23:39:57 -0300

    Logger final version
    * Logs files in the desktop-app/data/log directory;
    
    * Allows multiple levels and different filenames.

--- desktop-app/service/util/logger.ts
+++ desktop-app/service/util/logger.ts
@@ -1,9 +1,9 @@
-import { Logger, createLogger, format, transports, loggers } from 'winston';
+import { Logger, createLogger, format, transports, loggers, config } from 'winston';
 import { environment } from '../../environments';
 import { TransformableInfo } from 'logform';
-import { electronService } from '../electron.service';
 import * as electron from 'electron';
 import { join } from 'path';
+import { AbstractConfigSetLevels } from 'winston/lib/winston/config';
 
 /**
  * An implementation of a Logger using winston logger library.
@@ -23,7 +23,7 @@ export class GesthorLogger {
      * A constant reference to the debug message format.
      */
     private readonly printfFormat = format.printf((info: TransformableInfo) => {
-        return `${info.timestamp}[${info.label}] ${info.level}: ${info.message}`;
+        return `[${info.timestamp}][${info.level}][${info.label}] ${info.message}`;
     });
 
     /**
@@ -33,20 +33,26 @@ export class GesthorLogger {
 
     /**
      * A public constructor that instantiates this log instance. Creates an entry for
-     * a class named `className`, directed to the file `filename`.
+     * a class named `className`, directed to the file `filename`, with levels `levels`.
      * 
      * @param className The class name for which the log is used.
-     * @param filename The destination filename for the log.
+     * @param fileName The destination filename for the log.
+     * @param levels The levels to be logged in this instance.
      */
-    public constructor(private className: string = 'Generics', private filename: string = 'combined.log') {
-        this.logPath = join(electron.app.getAppPath(), '..', 'logs', this.filename);
+    public constructor(
+        public className: string = 'Generics', 
+        public fileName: string = 'combined.log', 
+        private levels: AbstractConfigSetLevels = config.npm.levels
+    ) {
+        this.logPath = join(electron.app.getAppPath(), 'desktop-app', 'data', 'logs', this.fileName);
         this.winstonLogger = createLogger({
+            levels: this.levels,
             format: format.combine(
                 format.label({ label: this.className }), format.timestamp(), format.splat(), this.printfFormat
             ),
             transports: [
                 new transports.File({
-                    filename: this.logPath
+                    filename: this.logPath,
                 })
             ]
         });
@@ -56,17 +62,27 @@ export class GesthorLogger {
     }
 
     /**
-     * Writes a message to the debug level.
+     * Writes a message in the error level.
      * 
      * @param message Message format, as a string.
      * @param params The message's params.
      */
-    public debug(message: string, ...params: any[]): void {
-        this.winstonLogger.log('debug', message, params);
+    public error(message: string, ...params: any[]): void {
+        this.winstonLogger.log('error', message, params);
     }
 
     /**
-     * Writes a message to the info level.
+     * Writes a message in the warn level.
+     * 
+     * @param message Message format, as a string.
+     * @param params The message's params.
+     */
+    public warn(message: string, ...params: any[]): void {
+        this.winstonLogger.log('warn', message, params);
+    }
+
+    /**
+     * Writes a message in the info level.
      * 
      * @param message Message format, as a string.
      * @param params The message's params.
@@ -76,13 +92,53 @@ export class GesthorLogger {
     }
 
     /**
-     * Writes a message to the error level.
+     * Writes a message in the http level.
      * 
      * @param message Message format, as a string.
      * @param params The message's params.
      */
-    public error(message: string, ...params: any[]): void {
-        this.winstonLogger.log('error', message, params);
+    public http(message: string, ...params: any[]): void {
+        this.winstonLogger.log('http', message, params);
+    }
+
+    /**
+     * Writes a message in the verbose level.
+     * 
+     * @param message Message format, as a string.
+     * @param params The message's params.
+     */
+    public verbose(message: string, ...params: any[]): void {
+        this.winstonLogger.log('verbose', message, params);
+    }
+
+    /**
+     * Writes a message in the debug level.
+     * 
+     * @param message Message format, as a string.
+     * @param params The message's params.
+     */
+    public debug(message: string, ...params: any[]): void {
+        this.winstonLogger.log('debug', message, params);
+    }
+
+    /**
+     * Writes a message in the silly level.
+     * 
+     * @param message Message format, as a string.
+     * @param params The message's params.
+     */
+    public silly(message: string, ...params: any[]): void {
+        this.winstonLogger.log('silly', message, params);
+    }
+
+    /**
+     * Writes a message in the `level` level.
+     * 
+     * @param message Message format, as a string.
+     * @param params The message's params.
+     */
+    public write(level: string, message: string, ...params: any[]): void {
+        this.winstonLogger.log(level, message, params);
     }
 
 }
