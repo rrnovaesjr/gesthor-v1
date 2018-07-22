9d9fb90a07222a203a3ef7c335dc37a3b0bf9197
Rodrigo Novaes <rodrigo.novaes.jr@gmail.com>
2018-07-22 18:45:12 -0300

    Inserting new comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+/**
+ * Application component.
+ * 
+ * @author rodrigo-novaes
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +13,16 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
+  /**
+   * The app title.
+   */
   title = 'Gesthor';
 
+  /**
+   * Constructs the new application.
+   * 
+   * @param translateService A translate service.
+   */
   public constructor(private translateService: TranslateService) {
     this.translateService.addLangs(['pt-br']);
     this.translateService.setDefaultLang('pt-br');
